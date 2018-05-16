0cc550b854a8d56ce5cb0fdcc92bdd8f2fc412ad
gwvdh <gwvdh97@gmail.com>
2018-05-16 13:15:21 +0200

    front: Battery

--- front/src/Battery.js
+++ front/src/Battery.js
@@ -5,33 +5,30 @@ import faBatteryQuarter from "@fortawesome/fontawesome-free-solid/faBatteryQuart
 import faBatteryHalf from "@fortawesome/fontawesome-free-solid/faBatteryHalf";
 import faBatteryThreeQuarters from "@fortawesome/fontawesome-free-solid/faBatteryThreeQuarters";
 import faBatteryFull from "@fortawesome/fontawesome-free-solid/faBatteryFull";
-import faAngleDown from "@fortawesome/fontawesome-free-solid/faAngleDown";
 
 // Properties: percentage
 const Battery = props => {
-  const BatteryIcon = properties => {
-    const percentage = properties.percentage;
-    if (percentage < 10) {
-      return (
-        <FontAwesomeIcon icon={faBatteryEmpty} rotation={270} color="red" />
-      );
-    } else if (percentage >= 10 && percentage < 40) {
-      return <FontAwesomeIcon icon={faBatteryQuarter} rotation={270} />;
-    } else if (percentage >= 40 && percentage < 60) {
-      return <FontAwesomeIcon icon={faBatteryHalf} rotation={270} />;
-    } else if (percentage >= 60 && percentage < 90) {
-      return <FontAwesomeIcon icon={faBatteryThreeQuarters} rotation={270} />;
-    } else {
-      return <FontAwesomeIcon icon={faBatteryFull} rotation={270} />;
-    }
-  };
+  const percentage = props.percentage;
   return (
     <div>
-      {" "}
-      <BatteryIcon icon={faBatteryFull} />
-      {props.percentage}%
+      {getBatteryIcon(percentage)}
+      {percentage}%
     </div>
   );
+};
+
+const getBatteryIcon = percentage => {
+  if (percentage < 10) {
+    return <FontAwesomeIcon icon={faBatteryEmpty} rotation={270} color="red" />;
+  } else if (percentage >= 10 && percentage < 40) {
+    return <FontAwesomeIcon icon={faBatteryQuarter} rotation={270} />;
+  } else if (percentage >= 40 && percentage < 60) {
+    return <FontAwesomeIcon icon={faBatteryHalf} rotation={270} />;
+  } else if (percentage >= 60 && percentage < 90) {
+    return <FontAwesomeIcon icon={faBatteryThreeQuarters} rotation={270} />;
+  } else {
+    return <FontAwesomeIcon icon={faBatteryFull} rotation={270} />;
+  }
 };
 
 export default Battery;
