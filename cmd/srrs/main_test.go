56b75048393d1dfe83e32d6d037da40bd5c1437d
Roman Volosatovs <rvolosatovs@riseup.net>
2018-05-16 12:11:01 +0200

    srrs: Add integration test

--- cmd/srrs/main_test.go
+++ cmd/srrs/main_test.go
@@ -1,6 +1,118 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
 
-func TestMain(t *testing.T) {
+	"github.com/gorilla/websocket"
+	"github.com/rvolosatovs/turtlitto"
+	"github.com/rvolosatovs/turtlitto/pkg/api"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	unixSockPath = filepath.Join(os.TempDir(), "trc-sock-test")
+
+	unixSock net.Listener
+)
+
+func init() {
+	f, err := os.Open(unixSockPath)
+	if !os.IsNotExist(err) {
+		f.Close()
+
+		log.Printf("Removing %s...", unixSockPath)
+		if err := os.Remove(unixSockPath); err != nil {
+			log.Fatalf("Failed to remove %s: %s", unixSockPath, err)
+		}
+	}
+
+	unixSock, err = net.Listen("unix", unixSockPath)
+	if err != nil {
+		log.Fatalf("Failed to open Unix socket on %s: %s", unixSockPath, err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	if err := flag.Set("socket", unixSockPath); err != nil {
+		log.Fatalf("Failed to set `socket` to %s: %s", unixSockPath, err)
+	}
+	go func() {
+		log.Print("Starting SRRS in goroutine...")
+		main()
+	}()
+
+	ret := m.Run()
+
+	if err := unixSock.Close(); err != nil {
+		log.Printf("Failed to close Unix socket: %s", err)
+	}
+
+	log.Printf("Exiting with return code: %d", ret)
+	os.Exit(ret)
+}
+
+func TestAll(t *testing.T) {
+	a := assert.New(t)
+
+	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost"+defaultAddr+"/state", nil)
+	if !a.Nil(err) {
+		t.FailNow()
+	}
+
+	state := map[string]*turtlitto.State{
+		"foo": &turtlitto.State{
+			ID: "bar",
+			// TODO: add more fields
+		},
+	}
+
+	go func() {
+		for {
+			c, err := unixSock.Accept()
+			a.Nil(err)
+
+			var req api.Message
+
+			err = json.NewDecoder(c).Decode(&req)
+			if !a.Nil(err) {
+				return
+			}
+
+			resp := &api.Message{
+				MessageID: req.MessageID,
+				Type:      req.Type,
+			}
+
+			switch req.Type {
+			case "set_state":
+			case "get_status":
+				b, err := json.Marshal(state)
+				if err != nil {
+					panic(err)
+				}
+				resp.Payload = b
+			case "command":
+			default:
+				t.Errorf("Unmatched message type: %s", req.Type)
+				return
+			}
+
+			err = json.NewEncoder(c).Encode(resp)
+			a.Nil(err)
+		}
+	}()
+
+	var got map[string]*turtlitto.State
+
+	err = conn.ReadJSON(&got)
+	if !a.Nil(err) {
+		t.FailNow()
+	}
+	a.Equal(state, got)
 }
