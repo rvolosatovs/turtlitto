dc8f29d742d7441c34cdab02bf6ebcedb7812f51
Bogdans Afonins <bogdans.afonins@gmail.com>
2018-05-23 16:07:36 +0200

    front: Add tests for TurtleEnableBar

---
+++ front/src/TurtleEnableBar/index.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import TurtleEnableBar from ".";
+import TurtleEnableButton from "./TurtleEnableButton";
+
+describe("TurtleEnableBar", () => {
+  it("should match snapshot", () => {
+    const wrapper = shallow(
+      <TurtleEnableBar turtles={[]} onEnable={() => {}} onDisable={() => {}} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render all turtles as <TurtleEnableButton /> components", () => {
+    const turtles = [
+      {
+        id: 0,
+        enabled: false
+      },
+      {
+        id: 1,
+        enabled: false
+      },
+      {
+        id: 2,
+        enabled: false
+      }
+    ];
+    const wrapper = mount(
+      <TurtleEnableBar
+        turtles={turtles}
+        onEnable={() => {}}
+        onDisable={() => {}}
+      />
+    );
+
+    expect(wrapper.find(TurtleEnableButton).length).toBe(turtles.length);
+  });
+});
