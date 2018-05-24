1342d87bef440a7762c846145bd19a9ab3265930
Bogdans Afonins <bogdans.afonins@gmail.com>
2018-05-24 14:31:36 +0200

    front: Refactor NotificationWindow

--- front/src/NotificationWindow/index.js
+++ front/src/NotificationWindow/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import faTimes from "@fortawesome/fontawesome-free-solid/faTimes";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import theme from "../theme";
 import PropTypes from "prop-types";
+import notificationTypes from "./notificationTypes";
 
 const Window = styled.div`
-  background-color: ${props => props.backgroundColor};
+  ${props => props.background};
   border-style: solid;
   border-width: 0.4rem;
   width: 90%;
@@ -14,38 +14,60 @@ const Window = styled.div`
   margin: 0 auto;
 `;
 
-const Header = styled.header`
+const ToolBar = styled.div`
+  display: flex;
+  padding: 1.5rem 1rem;
+  align-items: center;
+`;
+
+const Title = styled.h3`
   text-align: center;
-  font-size: 5rem;
+  font-size: 4rem;
+  text-transform: uppercase;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  flex-grow: 1;
+  margin: 0;
 `;
 
 const Text = styled.p`
   padding: 0 1rem;
   text-align: center;
-  font-size: 4rem;
+  font-size: 3rem;
   overflow-wrap: break-word;
 `;
 
 const CloseButton = styled.button`
   border: none;
   background-color: transparent;
-  position: absolute;
-  top: 0;
-  right: 0;
-  padding: 1rem 2rem;
   font-size: 2rem;
 `;
 
-const NotificationTypes = Object.freeze({
-  SUCCESS: "SUCCESS",
-  WARNING: "WARNING",
-  ERROR: "ERROR"
-});
+const getBackground = type => {
+  switch (type) {
+    case notificationTypes.SUCCESS:
+      return css`
+        background: ${props => props.theme.notificationSuccess};
+      `;
+    case notificationTypes.WARNING:
+      return css`
+        background: ${props => props.theme.notificationWarning};
+      `;
+    case notificationTypes.ERROR:
+      return css`
+        background: ${props => props.theme.notificationError};
+      `;
+    default:
+      throw new Error("Unknown notification type");
+  }
+};
 
 /**
  * Creates a notification window
  * Author: S.A. Tanja
  * Author: G.W. van der Heijden
+ * Author: B. Afonins
  *
  * Props:
  * - notificationType: the title and color selection
@@ -54,24 +76,16 @@ const NotificationTypes = Object.freeze({
  * - children: possible extra elements that could be included (buttons, etc)
  */
 const NotificationWindow = props => {
-  const backgroundColor = () => {
-    switch (props.notificationType) {
-      case NotificationTypes.SUCCESS:
-        return theme.notificationSuccess;
-      case NotificationTypes.WARNING:
-        return theme.notificationWarning;
-      case NotificationTypes.ERROR:
-        return theme.notificationError;
-      default:
-        return "white";
-    }
-  };
+  const background = getBackground(props.notificationType);
+
   return (
-    <Window backgroundColor={backgroundColor}>
-      <Header>{props.notificationType}</Header>
-      <CloseButton onClick={() => props.onDismiss()}>
-        <FontAwesomeIcon icon={faTimes} color="black" size="4x" />
-      </CloseButton>
+    <Window background={background}>
+      <ToolBar>
+        <Title>{props.notificationType}</Title>
+        <CloseButton onClick={() => props.onDismiss()}>
+          <FontAwesomeIcon icon={faTimes} color="black" size="4x" />
+        </CloseButton>
+      </ToolBar>
       <Text>{props.message}</Text>
       <div>{props.children}</div>
     </Window>
@@ -79,9 +93,10 @@ const NotificationWindow = props => {
 };
 
 NotificationWindow.propTypes = {
-  notificationType: PropTypes.oneOf(["SUCCESS", "WARNING", "ERROR"]),
+  notificationType: PropTypes.oneOf(Object.values(notificationTypes))
+    .isRequired,
   onDismiss: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired
 };
 
-export { NotificationTypes, NotificationWindow };
+export default NotificationWindow;
