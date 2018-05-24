f2876e011c6f2cce4150fc4bf49cea69a21026e0
Bogdans Afonins <bogdans.afonins@gmail.com>
2018-05-24 14:32:30 +0200

    front: Refactor NotificationWindow tests

--- front/src/NotificationWindow/index.test.js
+++ front/src/NotificationWindow/index.test.js
@@ -1,58 +1,48 @@
 import React from "react";
-import { NotificationTypes, NotificationWindow } from ".";
+import NotificationWindow from ".";
+import notificationTypes from "./notificationTypes";
 import renderer from "react-test-renderer";
 
 describe("NotificationWindow", () => {
-  it("shows a SUCCESS notification", () => {
-    const component = renderer.create(
-      <NotificationWindow
-        notificationType={NotificationTypes.SUCCESS}
-        onDismiss={() => {
-          console.log("Notification dismissed");
-        }}
-        message="This is a very annoying and extremely long error message which is displayed in full and hopefully uses wrap correctly with extremelylongwordswhichareuninterruptedandwithoutspaceswhichevenfillstheentirebox."
-      />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  describe("is success notification", () => {
+    it("should match snapshot", () => {
+      const component = renderer.create(
+        <NotificationWindow
+          notificationType={notificationTypes.SUCCESS}
+          onDismiss={() => {}}
+          message="notification message"
+        />
+      );
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
-  it("shows a WARNING notification", () => {
-    const component = renderer.create(
-      <NotificationWindow
-        notificationType={NotificationTypes.WARNING}
-        onDismiss={() => {
-          console.log("Notification dismissed");
-        }}
-        message="This is a very annoying and extremely long error message which is displayed in full and hopefully uses wrap correctly with extremelylongwordswhichareuninterruptedandwithoutspaceswhichevenfillstheentirebox."
-      />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+  describe("is warning notification", () => {
+    it("should match snapshot", () => {
+      const component = renderer.create(
+        <NotificationWindow
+          notificationType={notificationTypes.WARNING}
+          onDismiss={() => {}}
+          message="notification message"
+        />
+      );
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
-  it("shows a ERROR notification", () => {
-    const component = renderer.create(
-      <NotificationWindow
-        notificationType={NotificationTypes.ERROR}
-        onDismiss={() => {
-          console.log("Notification dismissed");
-        }}
-        message="This is a very annoying and extremely long error message which is displayed in full and hopefully uses wrap correctly with extremelylongwordswhichareuninterruptedandwithoutspaceswhichevenfillstheentirebox."
-      />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it("shows a non identified notification", () => {
-    const component = renderer.create(
-      <NotificationWindow
-        notificationType={NotificationTypes.SOMETHING}
-        onDismiss={() => {
-          console.log("Notification dismissed");
-        }}
-        message="This is a very annoying and extremely long error message which is displayed in full and hopefully uses wrap correctly with extremelylongwordswhichareuninterruptedandwithoutspaceswhichevenfillstheentirebox."
-      />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+
+  describe("is error notification", () => {
+    it("should match snapshot", () => {
+      const component = renderer.create(
+        <NotificationWindow
+          notificationType={notificationTypes.ERROR}
+          onDismiss={() => {}}
+          message="notification message"
+        />
+      );
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
