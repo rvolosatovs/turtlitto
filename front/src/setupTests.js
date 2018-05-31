7d2c510fa7dfabf8d50340c734c89255ffa3e405
Henk van der Laan <opensource@henkvdlaan.com>
2018-05-31 13:46:13 +0200

    front: Add WebSocket mock

--- front/src/setupTests.js
+++ front/src/setupTests.js
@@ -1,5 +1,8 @@
 // Setup enzyme. This file will processed by create-react-app automatically.
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { WebSocket } from "mock-socket";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.WebSocket = WebSocket;
