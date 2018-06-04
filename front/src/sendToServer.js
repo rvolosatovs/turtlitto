deeb7edd80f3024a49b7edf79dcd34f401f1cbaf
Franken <tom.franken311@gmail.com>
2018-06-04 15:19:38 +0200

    front: Made the sendToServer function

---
+++ front/src/sendToServer.js
@@ -0,0 +1,9 @@
+export default (message, destination) => {
+  const l = window.location;
+  console.log(`send to ${l.protocol}://${l.host}/api/v1/${destination}`);
+  return fetch(`${l.protocol}://${l.host}/api/v1/${destination}`, {
+    method: "PUT",
+    headers: {},
+    body: JSON.stringify(message)
+  });
+};
