a6668a7683172deec3c82a6a659dfe8e6d07188b
Henk van der Laan <opensource@henkvdlaan.com>
2018-06-12 16:54:03 +0200

    front: Improve coverage of NotificationWindow

--- front/src/NotificationWindow/index.test.js
+++ front/src/NotificationWindow/index.test.js
@@ -44,4 +44,42 @@ describe("NotificationWindow", () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("throws an error when an unknown notification type is shown", () => {
+    const notification = {
+      notificationType: "annoying",
+      message: "Crash pls"
+    };
+    // Hide PropTypes validation error, we want the normal error
+    const oldError = console.error;
+    console.error = () => {};
+    expect(() => {
+      shallow(
+        <NotificationWindow notification={notification} onDismiss={() => {}} />
+      );
+    }).toThrow();
+    console.error = oldError;
+  });
+
+  it("can be dismissed", () => {
+    const dismissFn = jest.fn();
+    const notification = {
+      notificationType: notificationTypes.ERROR,
+      message: "Pants on FIRE"
+    };
+    const wrapper = shallow(
+      <NotificationWindow
+        notification={notification}
+        onDismiss={() => {
+          dismissFn();
+        }}
+      />
+    );
+    expect(dismissFn).not.toBeCalled();
+    wrapper
+      .find("FontAwesomeIcon")
+      .parent() // Needed because under shallow, events don't bubble
+      .simulate("click");
+    expect(dismissFn).toBeCalled();
+  });
 });
