6bda34d970440e2f934822f71661128f8646ea4c
Bogdans Afonins <bogdans.afonins@gmail.com>
2018-06-19 14:33:03 +0200

    front: Refactor TurtleEnableButton tests

--- front/src/TurtleEnableBar/TurtleEnableButton.test.js
+++ front/src/TurtleEnableBar/TurtleEnableButton.test.js
@@ -1,13 +1,39 @@
 import React from "react";
 import TurtleEnableButton from "./TurtleEnableButton";
 import { shallowWithTheme } from "../testUtils";
+import theme from "../theme";
 
 describe("TurtleEnableButton", () => {
-  it("should match snapshot", () => {
-    const wrapper = shallowWithTheme(
-      <TurtleEnableButton enabled id={1} onTurtleEnableChange={() => {}} />
-    ).dive();
+  describe("is active", () => {
+    it("should match snapshot", () => {
+      const wrapper = shallowWithTheme(
+        <TurtleEnableButton enabled id={1} onTurtleEnableChange={() => {}} />
+      ).dive();
 
-    expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toHaveStyleRule("background-color", theme.buttonActive);
+      expect(wrapper).toHaveStyleRule("border-color", "none");
+      expect(wrapper).toHaveStyleRule(
+        "border-style",
+        theme.buttonBorderStyleActive
+      );
+    });
+  });
+
+  describe("is inactive", () => {
+    it("should match snapshot", () => {
+      const wrapper = shallowWithTheme(
+        <TurtleEnableButton
+          id={1}
+          enabled={false}
+          onTurtleEnableChange={() => {}}
+        />
+      ).dive();
+
+      expect(wrapper).toMatchSnapshot();
+      expect(wrapper).toHaveStyleRule("background-color", theme.button);
+      expect(wrapper).toHaveStyleRule("border-color", theme.buttonBorder);
+      expect(wrapper).toHaveStyleRule("border-style", theme.buttonBorderStyle);
+    });
   });
 });
